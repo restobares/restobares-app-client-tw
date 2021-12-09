6bb29cbbde0e8ab88cbbacf1a0e11a08cd2c609e
Juan Canelon <liscanojac@gmail.com>
2021-12-08 21:32:48 -0300

    Setting up ActiveOrders component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage.jsx'
+import ActiveOrders from './components/ActiveOrders/ActiveOrders.jsx';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="global">
         <Routes>
           <Route path="/home" element={<LandingPage />}></Route>
+          <Route path="/ordenes" element={<ActiveOrders />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
--- src/components/ActiveOrders/ActiveOrders.jsx
+++ src/components/ActiveOrders/ActiveOrders.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
 
 const ActiveOrders = () => {
+  
+  const dummyOrders = [
+    {
+      orderId: 1,
+      tableNumber: 2,
+      orderTime: "15:48",
+      // this has to have name and quatity of the orders
+      orderItems: [
+        {
+          name: "hamburgesa de pollo",
+          quantity: 2
+        },
+        {
+          name: "ensalada",
+          quantity: 1
+        }
+      ],
+      comments: "la ensalada sin aceite de oliva"
+    },
+    {
+      orderId: 2,
+      tableNumber: 4,
+      orderTime: "15:50",
+      orderItems: [
+        {
+          name: "budin",
+          quantity: 1
+        }
+      ],
+      comments: ""
+    }
+  ];
+  
   return (
-    <div>
+    <div className="w-10/12 md:w-8/12 lg:7/12 mx-auto relative py-20">
       
     </div>
   )
