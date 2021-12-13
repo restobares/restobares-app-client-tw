7e89235ec2fb8127a7ebd2b53e49e6d65b322c82
kisser91 <nahuelkisser91@gmail.com>
2021-12-13 04:22:40 -0300

    esto es un lorem la puta que te pario

--- src/components/AuxiliarComponents/OrderCard.jsx
+++ src/components/AuxiliarComponents/OrderCard.jsx
@@ -27,35 +27,38 @@ const OrderCard = ({ platillo }) => {
   };
 
   return (
-    <div className="container mt-5 mb-3 float-left rounded-br-lg bg-pink-400 text-white space-x-0.5">
-      <div className="float-left m-0 object-cover">
-        <img
+    <div className="flex  mr-8 mt-5  h-60 float-right  bg-pink-400 text-white rounded-br-3xl w-11/12 ">
+      <div className="float-left w-80">
+        <img className="object-cover min-w-full min-h-full"
           src={img}
           style={{ width: 100, height: 100, borderRadius: 5 }}
           alt=""
         />
       </div>
-      <div className="float-left ml-2 mr-3">
-        <p className="text-paragraph"> {product_name} </p>
-        <p className="float-left mt-1"> {details} </p>
-      </div>
-      <div className="float-left">
-        <p className="mt-1 mb-8"> ${price} </p>
+      <div className="bg-pink-500 h-60 w-80 float-rightt ">
+        <p className=" float-left ml-2 mt-2 text-3xl "> {product_name} </p>
+        <div className="float-right bg-pink-600 w-24 h-10 rounded-bl-xl ">
+        <p className="text-right text-2xl mt-1 mr-2"> ${price} C/U </p>
+        </div>
+        <div className="h-36 bg-pink-400 mt-12 mr-2 text-xl rounded-xl">
+        <p className="float-left mt-1 ml-2"> {details} </p>
+        </div>
+      <div className="float-ri">
+        <div className="float-left  w-48 bg-pink-700 text-xl h-8 mt-2 rounded-md px-2 mx-2">
+        <h1 className="inline-block mt-1"> Cantidad:   </h1><h1 className=" inline-block ml-4 "> {cart[product_id] ? cart[product_id].quantity : 0}</h1><h1 className=" inline-block ml-4 pr-2 "> XXXX $</h1>
+        </div>
         <button
-          className="bg-pink-500 rounded-md mt-4 mb-2 px-2 mr-2"
+          className=" bg-pink-700 h-8 text-2xl rounded-md mt-2 mb-2 px-2 mr-2 ml-4"
           type="button"
-          onClick={addPlatillo}
-        >
-          Agregar
-        </button>
+          onClick={addPlatillo}> Agregar </button>
       </div>
-      <div className="rounded-br-lg float-left plus-minus">
-        <button className="mt-4" onClick={add}>
+      </div>
+      <div className="bg-pink-700 w-16 float-right rounded-br-3xl ">
+        <button className="mt-12 text-center text-2xl" onClick={add}>
           ➕
         </button>
-        <h1>{cart[product_id] ? cart[product_id].quantity : 0}</h1>
         <div className="pl-2 flex item-center">
-        <button className="mt-0" disabled={!cart[product_id] || cart[product_id]. quantity === 0} onClick={minus}>➖</button>
+        <button className="mt-12 mx-auto" disabled={!cart[product_id] || cart[product_id]. quantity === 0} onClick={minus}>➖</button>
         </div>
       </div>
     </div>
--- src/components/ChildrenComponents/OrdersCarousel.jsx
+++ src/components/ChildrenComponents/OrdersCarousel.jsx
@@ -10,9 +10,9 @@ const OrdersCarousel = ({ pedido, currentOrder }) => {
     <div className="mb-auto">
       {addOrder.map((el) => {
         return (
-          <div className="container mt-5 mb-3 float-left rounded-br-lg bg-pink-400 text-white space-x-0.5">
-            <div className="float-left m-0 ">
-              <img
+          <div className="flex float-right mt-5 w-11/12 h-60 bg-pink-400  text-white rounded-br-3xl mr-8">
+            <div className="float-left w-80 ">
+              <img className="object-cover min-w-full min-h-full "
                 src={el.img}
                 style={{ width: 100, height: 100, borderRadius: 5 }}
                 alt=""
@@ -20,13 +20,17 @@ const OrdersCarousel = ({ pedido, currentOrder }) => {
             </div>
             {/*Titulo*/}
 
-            <div className="flex flex-row ml-4 mt-1 text-left align-items-start">
+            <div className="">
               <div>
-                <p>{el.product_name}</p>
-                <p>{el.details}</p>
-                <p>{el.price}</p>
+              <p className=" text-3xl bg-pink-700 w-96"> {el.product_name} </p>
+              <div className="clear-left bg-pink-500 mt-2 mr-2 text-2xl rounded-xl">
+                <p className="float-left mt-1 ml-2"> {el.details} </p>
               </div>
-              <div className="bg-pink-500 w-10 h-6 mx-1">Pedido</div>
+              <div className="float-left w-48 mt-28 py-1 ml-2 rounded-md bg-pink-700 px-4 text-2xl ">
+              <p className="float-right ml-4"> $</p><p className="float-right">{el.price}</p> <p className="float-left mr-2"> Cant XXX</p>
+              </div>
+              </div>
+              <div className="float-right  mt-28 py-1 mr-4 rounded-md rounded-br-xl bg-pink-500 px-4 text-2xl">Pedido</div>
               {/* {
 			  pedido
 			  ? <div className="bg-pink-500 w-10 h-6 mx-1">Pedido</div>
