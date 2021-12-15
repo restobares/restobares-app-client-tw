2a778af24d2a564a5d30500436a25a5abab502e4
chardhm <chych1317@gmail.com>
2021-12-15 13:50:37 -0600

    sidebar

--- src/components/ChildrenComponents/OrderBar.jsx
+++ src/components/ChildrenComponents/OrderBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from "react-router-dom";
 import Search from "../AuxiliarComponents/Search";
+import SideBar from '../ChildrenComponents/SideBar';
 
 const OrderBar = () => {
   let idTable = 5;
@@ -9,9 +10,11 @@ const OrderBar = () => {
   return (
     <div className='nav-bar'>
         <div className=''>
+        <SideBar>
         <button className="float-left button ml-1">
         <img src="https://img.icons8.com/material-outlined/24/aa0020/menu--v1.png"  width="24" className='ml-1'/>
         </button>
+        </SideBar>
         <Search/>
         <Link  to={`/resto/${idResto}/table/${idTable}/order`}> 
         <button className="float-right mr-2 button">
