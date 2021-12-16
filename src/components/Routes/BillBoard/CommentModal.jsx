f5e0bf6c98c01698eddc4c2fdc2f7c1df795309e
MateolLopez <80400141+MateolLopez@users.noreply.github.com>
2021-12-16 20:08:29 -0300

    CommandModal y boton pedir (falta actualizar estado)

--- src/components/Routes/BillBoard/CommentModal.jsx
+++ src/components/Routes/BillBoard/CommentModal.jsx
@@ -1,34 +1,85 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { addOrderToCart } from "../../../redux/actions";
 
-const Modal = ({showModal, setShowModal}) => {
-    return (
+const CommentModal = ({ showModal, setShowModal }) => {
+
+
+	const {cart}= useSelector((state)=>state);
+
+     const dispatch = useDispatch();
+
+    const [input, setInput] = useState ({
+        comment: ''
+    })
+
+
+    const pedir = () => {
+      dispatch(addOrderToCart());
+    };
+    
+    const changeModal = () => {
+        setShowModal((prev) => !prev);
+        pedir()
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+      };
+
+    const handleChange = (e) => {
+        setInput({
+        	...input,
+        	[e.target.name]: e.target.value
+    	})
+    }
+
+    
+
+  return (
     <>
-    {showModal ?
-      <div id="modal-component-container" className="fixed inset-0" >
-        <div className="modal-flex-container flex items-end justify-center min-h-screen pt-4 px-4 pb-60 text-center sm:block sm:p-0">
-          <div className="modal-bg-container fixed inset-0 bg-gray-700 bg-opacity-75 pt-60">
-  
-            <div id="modal-container" className="modal-container inline-block align-bottom rounded-lg overflow-hidden transform transition-all sm:my-8 sm:align-middle sm:max-w-lg w-full">
-             <div className="modal-wrapper sm:p-6 sm:pb-4">
-              <div className="modal-wrapper-flex sm:flex sm:items-start">
-        
-             <div className="p-2 mt-10 inline-block">
-               <div className="w-60 h-60 bg-pink-700 rounded-full border-2 border-pink-900 sm:w-auto">
-                 <h1 className='pt-5'></h1>
-                 <h1 className='modal-content text-2xl text-red-50 font-bold'>Quiere Añadir un comentario?</h1>
-                 <button onClick={() => setShowModal(prev => !prev)} className="bg-red-500 rounded-xl px-4 mt-8">Cerrar</button>
-               </div>
-             </div>
-         
+      {showModal ? (
+        <div id="modal-component-container" className="fixed inset-0">
+          <div className="modal-flex-container flex items-end justify-center  pt-4 px-4 pb-60 text-center sm:block sm:p-0">
+            <div className="modal-bg-container fixed inset-0 bg-gray-700 bg-opacity-75 pt-60">
+              <div
+                id="modal-container"
+                className="modal-container inline-block align-bottom rounded-lg overflow-hidden transform transition-all sm:my-8 w-full"
+              >
+                <div className="modal-wrapper sm:p-6 sm:pb-4">
+                  <div className="modal-wrapper-flex sm:flex sm:items-start">
+                    <div className="p-2 mt-10 inline-block">
+                      <div className="w-60 h-max bg-pink-700 rounded-lg border-2 border-pink-900 sm:w-auto">
+                        <h1 className="pt-5"></h1>
+                        <h1 className="modal-content text-2xl text-red-50 font-bold"></h1>
+                        <h2>Desea añadir un comentario?</h2>
+                                             
+                        
+                        <form onSubmit={handleSubmit}>
+                            <label>
+                                <textarea onChange={(e)=>handleChange(e)} className="bg-pink-500 mt-4" type="text" name="comment" />
+                            </label>
+                            <button onClick={changeModal} 
+                                className="bg-red-500 rounded-xl px-4 mt-4">Enviar</button>
+                            </form>
+
+                        <button
+                          onClick={changeModal}
+                          className="bg-red-500 rounded-xl px-4 my-4"
+                        >
+                          No
+                        </button>
+                        
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-             </div>
             </div>
-          
           </div>
         </div>
-      </div> : null}
+      ) : null}
     </>
-    )
-}
+  );
+};
 
-export default Modal
+export default CommentModal;
