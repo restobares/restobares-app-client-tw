b4138f3901897944ef5b1b4562397568b7818862
Juan Canelon <liscanojac@gmail.com>
2021-12-21 11:24:47 -0300

    Setting filter logics

--- src/redux/reducers/cartOrder.js
+++ src/redux/reducers/cartOrder.js
@@ -1,44 +1,44 @@
-import { ActionTypes } from "../constants";
+// import { ActionTypes } from "../constants";
 
-const initialState = {order: "default"};
+// const initialState = {order: "default"};
 
-function cartOrder(state = initialState, action) {
-    //// NO FUNCIONA --> NO SE PUEDE ACCEDER A MENUS
-  if (action.type === ActionTypes.SET_ORDER) {
-    let order = action.payload;
-    console.log("order",order)
-    let orderedList;
-    console.log("state =>",state);
+// function cartOrder(state = initialState, action) {
+//     //// NO FUNCIONA --> NO SE PUEDE ACCEDER A MENUS
+//   if (action.type === ActionTypes.SET_ORDER) {
+//     let order = action.payload;
+//     console.log("order",order)
+//     let orderedList;
+//     console.log("state =>",state);
 
-    order !== "default" ? orderedList = state.menus.menu
-    .sort(function(a,b){
-      if (action.payload === "mayor"){
-          if(a.price < b.price){
-              return 1;
-          }
-          if(b.price < a.price){
-              return -1;
-          }
-          return 0
-      }
-      else{
-          if(a.price < b.price){
-              return 1;
-          }
-          if(b.price < a.price){
-              return -1;
-          }
-          return 0
-      }
-  })
+//     order !== "default" ? orderedList = state.menus.menu
+//     .sort(function(a,b){
+//       if (action.payload === "mayor"){
+//           if(a.price < b.price){
+//               return 1;
+//           }
+//           if(b.price < a.price){
+//               return -1;
+//           }
+//           return 0
+//       }
+//       else{
+//           if(a.price < b.price){
+//               return 1;
+//           }
+//           if(b.price < a.price){
+//               return -1;
+//           }
+//           return 0
+//       }
+//   })
 
-  : orderedList = state.menus.backupMenu
-
-
+//   : orderedList = state.menus.backupMenu
 
 
-    return {order, menu: orderedList }
-  }
-  return state;
-}
-export default cartOrder;
+
+
+//     return {order, menu: orderedList }
+//   }
+//   return state;
+// }
+// export default cartOrder;
