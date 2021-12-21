8943983cd4e58ef306fcb03dc16da0cbf87366b7
MateolLopez <80400141+MateolLopez@users.noreply.github.com>
2021-12-21 20:19:24 -0300

    Ya no se superpone el cartel de en camino, boton de cerrar el modal y cositas

--- src/components/Routes/OrderBoard/OrderBoard.jsx
+++ src/components/Routes/OrderBoard/OrderBoard.jsx
@@ -1,45 +1,45 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { getMenu, getLabels, getCategories } from '../../../redux/actions';
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getMenu, getLabels, getCategories } from "../../../redux/actions";
 
 //componente platillo
-import OrderCard from '../../AuxiliarComponents/OrderCard';
-import OrderBar from '../../ChildrenComponents/OrderBar';
-import SideBar from '../../ChildrenComponents/SideBar';
+import OrderCard from "../../AuxiliarComponents/OrderCard";
+import MenuPayBar from "../../ChildrenComponents/MenuPayBar";
+import OrderBar from "../../ChildrenComponents/OrderBar";
+import PayBar from "../../ChildrenComponents/PayBar";
+import SideBar from "../../ChildrenComponents/SideBar";
 
-
-export default function OrderBoard () {
-
+export default function OrderBoard() {
   const { idResto, idTable } = useParams();
 
   const dispatch = useDispatch();
-  const menu = useSelector((state) => state.menus.menu)
+  const menu = useSelector((state) => state.menus.menu);
 
   useEffect(() => {
-      dispatch(getLabels())
-    dispatch(getCategories())
-    dispatch(getMenu(idResto, idTable))
-  }, [dispatch])
+    dispatch(getLabels());
+    dispatch(getCategories());
+    dispatch(getMenu(idResto, idTable));
+  }, [dispatch]);
 
-    return (
-      <div className='pt-12'>
-        <OrderBar/>
-        <div className='fixed min-h-screen right-0 left-0 flex  '>
-            <SideBar/>
-            <div className='w-full pb-16 mx-2 mr-2 mt-2 h-screen flex flex-col overflow-auto '>
-            {menu && menu.map((product) =>(
-            <OrderCard
-              product_id={product.id}
-              key={product.id}
-              product={product}
-            />
+  return (
+    <div className="pt-12">
+      <OrderBar />
+      <div className="fixed min-h-screen right-0 left-0 flex  ">
+        <SideBar />
+        <div className="w-full pb-16 mx-2 mr-2 mt-2 h-screen flex flex-col overflow-auto ">
+          {menu &&
+            menu.map((product) => (
+              <OrderCard
+                product_id={product.id}
+                key={product.id}
+                product={product}
+              />
             ))}
-            </div>
         </div>
+        <MenuPayBar/>
       </div>
-	)
-
+    </div>
+  );
 }
-
