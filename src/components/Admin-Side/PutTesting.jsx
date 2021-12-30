319c3c73f0e72a7d31ba910a1130137b2753609e
Juan Canelon <liscanojac@gmail.com>
2021-12-30 15:46:41 -0300

    put and delete actions

--- src/components/Admin-Side/PutTesting.jsx
+++ src/components/Admin-Side/PutTesting.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { putTableEating } from '../../redux/actions';
+import { deleteProductFromTable, putTableEating } from '../../redux/actions';
 
 const PutTesting = () => {
   const { idResto } = useParams();
   const dispatch = useDispatch();
   const testToken = "AdminSupremeTest";
   const testTable = 4;
+  const testProduct = 1;
+  const testQuantity = 1;
 
   const putAction = async () => {
     dispatch(putTableEating(idResto, testTable, testToken))
   }
+
+  const deleteAction = () => {
+    dispatch(deleteProductFromTable(idResto, testTable, testProduct, testQuantity, testToken))
+  }
   return (
     <div>
       <button onClick={putAction}>PUT</button>
+      <br />
+      <br />
+      <br />
+      <button onClick={deleteAction}>DELETE</button>
     </div>
   )
 }
