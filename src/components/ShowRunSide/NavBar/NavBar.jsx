8dc323a25f16a38c8282b51272b22f819ebe6298
Smesaz <semesile@gmail.com>
2022-01-04 17:49:12 -0500

    Sebas/DIngbell showRun advance 2

--- src/components/ShowRunSide/NavBar/NavBar.jsx
+++ src/components/ShowRunSide/NavBar/NavBar.jsx
@@ -1,12 +1,30 @@
 import React, { useEffect } from "react";
-import { Nav, NavbarContainer, NavLogo } from "./NavbarElements";
+import {FaBars} from "react-icons/fa"
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from "./NavbarElements";
 
 const NavBar = () => {
   return (
     <>
       <Nav>
         <NavbarContainer>
-          <NavLogo to ='/resto'>canelon</NavLogo>
+          <NavLogo to ='/resto'>DingBell</NavLogo>
+          <MobileIcon>
+              <FaBars/>
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to='about'>About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='services'>Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='singup'>Sing Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+              <NavBtnLink to='/singn'>Sing In</NavBtnLink>
+          </NavBtn>
         </NavbarContainer>
       </Nav>
     </>
