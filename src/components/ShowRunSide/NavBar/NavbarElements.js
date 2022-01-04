8501a93a097e15f9884b8128522f25cdae7815b2
brianbacca <brianbn05@gmail.com>
2022-01-04 18:04:31 -0300

    Brian/navBar

---
+++ src/components/ShowRunSide/NavBar/NavbarElements.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Nav = styled.nav`
+  background: #000;
+  /* height: 800px; */
+  /* margin-top: -80px; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+
+  @media screen and(max-width:960px) {
+    transition: 0.8s all ease;
+  }
+`;
+
+export const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  z-index: 1;
+  width: 100%;
+  padding: 0 24px;
+  max-width: 110px;
+`;
+
+export const NavLogo = styled(Link)`
+  color: red;
+  justify-self: flex-start;
+  cursor: pointer;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  margin-left: 24px;
+  font-weight: bold;
+  text-decoration: none;
+`;
