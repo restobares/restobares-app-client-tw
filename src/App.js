17af8d60eee5dcb54ea8095416cc32c174c9289c
MateolLopez <80400141+MateolLopez@users.noreply.github.com>
2022-01-04 20:41:24 -0300

    QrManagement y QrGenerated, falta arreglar cuando imprimis más de 9 mesas a la vez

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import AdminMenu from './components/Admin-Side/AdminMenu/AdminMenu.jsx';
 import QrManager from './components/Admin-Side/Settings/QrManagement/QrManagement.jsx';
 import Analytics from './components/Admin-Side/Settings/Analytics.jsx';
 import Themes from './components/Admin-Side/Settings/Themes.jsx';
+import QrGenerated from './components/Admin-Side/Settings/QrManagement/QrGenerated.jsx';
 
 
 function App() {
@@ -41,8 +42,10 @@ function App() {
           <Route path="resto/:idResto/put" element={<PutTesting />}></Route>
           <Route path="resto/:idResto/resto-home/menu" element={<AdminMenu />}></Route>
           <Route path="resto/:idResto/resto-home/qrmanager" element={<QrManager />}></Route>
+          <Route path="resto/:idResto/resto-home/qrmanager/qrs" element={<QrGenerated />}></Route>
           <Route path="resto/:idResto/resto-home/analytics" element={<Analytics />}></Route>
           <Route path="resto/:idResto/resto-home/themes" element={<Themes />}></Route>
+          
 
 
         </Routes>
