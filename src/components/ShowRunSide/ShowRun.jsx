df43a83bd8f954d4affdf93121b219f553e6ca76
Smesaz <semesile@gmail.com>
2022-01-04 14:18:55 -0500

    ShowRun beginning

--- src/components/ShowRunSide/ShowRun.jsx
+++ src/components/ShowRunSide/ShowRun.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 export default function ShowRun () {
   
     return (<div>
-        
+        <h1>Show Run DIngbell App</h1>
       </div>
     );
 }
