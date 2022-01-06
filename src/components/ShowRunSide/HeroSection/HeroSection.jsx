9e09a7bff63804e44e612913b47f505074c60ae0
brianbacca <brianbn05@gmail.com>
2022-01-05 23:42:19 -0300

    merge

--- src/components/ShowRunSide/HeroSection/HeroSection.jsx
+++ src/components/ShowRunSide/HeroSection/HeroSection.jsx
@@ -1,30 +1,48 @@
-import React from 'react'
-import {HeroContainer, HeroBg, HeroContent, VideoBg} from './HeroSectionElements.js';
-import video from '../../../video/video.mp4';
-import { Button } from 'react-scroll';
+import React, { useState } from "react";
+import {
+  HeroContainer,
+  HeroBg,
+  HeroContent,
+  VideoBg,
+  HeroH1,
+  HeroP,
+  HeroBtnWrappe,
+  ArrowRigth,
+  ArrowForward,
+} from "./HeroSectionElements.js";
+import video from "../../../video/video.mp4";
+import { Button } from "react-scroll";
 function HeroSection() {
-    return (
-        <HeroContainer>
-            <HeroBg>
-                <VideoBg autoPlay loop muted src={video} type='video/mp4'/>
-            </HeroBg>
-            {/* <HeroContent>
-                <HeroH1>
+  const [hover, setHover] = useState(false);
 
-                </HeroH1>
-                <HeroP>
-
-                </HeroP>
-                <HeroBtnWrapper>
-                    <Button to='/resto/register'>
-                        Get Started {hover ? <ArrowForward/>:<ArrowRigth/>}
-                    </Button>
-                </HeroBtnWrapper>
-            </HeroContent> */}
-        </HeroContainer>
-
-            
-    )
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Dingebell</HeroH1>
+        <HeroP>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam amet
+          tenetur nobis enim deleniti quibusdam accusamus hic reiciendis illum.
+          Repellendus nostrum vero modi consectetur molestias, amet quam debitis
+          sint nemo.
+        </HeroP>
+        <HeroBtnWrappe>
+          <Button
+            to="/resto/register"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRigth />}
+          </Button>
+        </HeroBtnWrappe>
+      </HeroContent>
+    </HeroContainer>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
