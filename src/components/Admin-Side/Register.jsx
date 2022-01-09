84956c986a9021958f8054fa9b59ad8ec63f722e
Smesaz <semesile@gmail.com>
2022-01-09 18:28:46 -0500

    updated

--- src/components/Admin-Side/Register.jsx
+++ src/components/Admin-Side/Register.jsx
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import Swal from "sweetalert2";
-import { inputValidatorRegister, register } from '../../redux/actions';
-
-const Register = () => {
-    const dispatch = useDispatch();
-    const bgimg = "https://houseofruthinc.org/wp-content/uploads/2019/04/dinner.jpg";
-    const [errors, setErrors] = useState({});
-    
-    const [input, setInput] = useState({
-        title: "",
-        email: "",
-        passAdmin: "",
-        passStaff: "",
-        logo: "",
-        tables: "",
-        payment_mp: "",
-      });
-=======
 import React from "react";
 import { useState, useEffect } from "react";
 // import { useNavigate, Link } from 'react-router-dom';
@@ -36,7 +14,6 @@ const Register = () => {
   const [passwordRepShown, setPasswordRepShown] = useState(false);
   const [passwordStaffShown, setPasswordStaffShown] = useState(false);
   const [passwordStaffRepShown, setPasswordStaffRepShown] = useState(false);
->>>>>>> 63ed455ea4192437a19e0e2b4bbaff084ebf1011
 
   const dispatch = useDispatch();
   // const navigate = useNavigate();
@@ -194,107 +171,6 @@ const Register = () => {
     });
   };
 
-<<<<<<< HEAD
-    return (
-        <div className='min-h-screen  flex bg-gray-bg1' style={{ 
-            backgroundImage: `url(${bgimg})`,
-            backgroundSize: "cover", 
-          }} >
-              <div className='inline-block mx-auto my-auto justify-items-center '>
-                <div className=' mx-2 bg-white rounded-lg border border-primaryBorder shadow-default py-10 px-16'>
-                <h1 className="m-5 text-lg font-bold">Restaurant Register 📝</h1>
-
-<form className="w-76 h-40 inline-block">
-  <input
-    type="text"
-    name="title"
-    maxLength="50"
-    className="text-center block mb-4 w-full px-5 py-3 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none" /* form-control */
-    placeholder="Restaurant Name"
-    value={input.title}
-    onChange={(e) => handleInputChanges(e)}
-  />
-
-  <input
-    id="email"
-    type="email"
-    name="email"
-    placeholder='Your Email'
-    value={input.email}
-    className="text-center block mb-4 w-full px-5 py-3 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
-    onChange={(e) => handleInputChanges(e)}
-  />
-
-  <input
-    type="password"
-    id='password'
-    name="passAdmin"
-    className="text-center block mb-4 w-full px-5 py-3 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
-    placeholder='Your Admin-Password'
-    value={input.passAdmin}
-    onChange={(e) => handleInputChanges(e)}
-  />
-  <input
-    type="password"
-    id='password'
-    name="passStaff"
-    className="text-center block mb-4 w-full px-5 py-3 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
-    placeholder='Your Staff-Password'
-    value={input.passStaff}
-    onChange={(e) => handleInputChanges(e)}
-  />
-  <input
-    type="number"
-    id='tables'
-    name="tables"
-    className="text-center block mb-4 w-full px-5 py-3 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
-    placeholder='Number of tables in your local'
-    value={input.tables}
-    min="1"
-    maxLength="4"
-    oninput="validity.valid||(value=value.replace(/\D+/g, 0))"
-    pattern='^[0-9]+'
-    onKeyUp={Number(input.tables) < 0 ? Number(input.tables) * -1 : Number(input.tables)!==0?input.tables: input.tables=1}
-    onChange={(e) => handleInputChanges(e)}
-  />
-  <input
-    type="file"
-    id="logo"
-    name="logo"
-    className="block mb-4 w-full px-5 py-3 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
-    accept="image/*"
-    onChange={(e) => handleImageSelection(e)}
-  />
-  <input
-    type="text"
-    name="payment_mp"
-    maxLength="50"
-    className="text-center block mb-4 w-full px-5 py-3 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none" /* form-control */
-    placeholder="Token Mercado Pago"
-    value={input.payment_mp}
-    onChange={(e) => handleInputChanges(e)}
-  />
-
-  {Object.keys(errors).length > 0 || input.title === "" ?
-  <button
-    type="submit"
-    onClick={notAlert}
-    className="text-white bg-gray-600 mt-4  w-32 px-4 py-2 rounded-3xl text-sm font-semibold"
-  >
-    Check In
-  </button>:
-  <button
-    type="submit"
-    onClick={alert}
-    className="mt-4 mb-4 bg-pink-700 w-32 px-4 py-2 rounded-3xl text-sm text-white font-semibold each-in-out "
-  >
-    Check In
-  </button> 
-  }
-                </form>
-                </div>
-              </div>
-=======
   const togglePassword = (e) => {
     e.preventDefault();
     setPasswordShown(!passwordShown);
@@ -452,7 +328,6 @@ const Register = () => {
               </button>
             )}
           </form>
->>>>>>> 63ed455ea4192437a19e0e2b4bbaff084ebf1011
         </div>
       </div>
     </div>
