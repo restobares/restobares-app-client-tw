9edcd969e7617cac18296c9e3680c66d48019f52
kisser91 <nahuelkisser91@gmail.com>
2022-01-12 18:55:04 -0300

    modal and other fixes

--- src/components/Routes/BillBoard/CommentModal.jsx
+++ src/components/Routes/BillBoard/CommentModal.jsx
@@ -37,31 +37,33 @@ const CommentModal = ({ showModal, setShowModal }) => {
     <>
       {showModal ? (
       <div className="fixed inset-0 bg-black bg-opacity-40 pt-36">
-      <div className="relative w-8/12 mx-auto h-72 bg-pink-700  border-2 border-gray-700 rounded-3xl">
-        <button onClick={(e) => backTrack(e)}className="absolute -top-3 -right-3">
-          <p className="text-xl bg-gray-300 pb-1 px-3 rounded-full">x</p>
-        </button>
-        <h1 className=' mt-8 text-base text-white mb-2'>Would you like to add a comment?</h1>
-      <div className="mx-4">
-        <label>
-          <textarea onChange={(e)=> setComment(e.target.value)} className="bg-gray-200 bg-opacity-50 w-full h-36 rounded-xl  text-xl px-1.5 resize-none" maxLength="100"></textarea>
-        </label>
-        <div className="inline-block w-full ">
-          {comment === "" ? (
-          <button className="float-left bg-gray-600 text-white cursor-not-allowed rounded-md ml-2 px-4 mt-2 ">
-            Send
-          </button>
-          ) : (
-            <button onClick={(e) => changeModal(e)}className="float-left bg-gray-200 rounded-md ml-2 px-4 mt-2 ">
-            Send
-          </button>
-          )}
-          <button onClick={(e) => changeModal(e)} className="float-right bg-gray-200 rounded-md mr-2 px-4 mt-2">
-            No
-          </button>
+        <div className="w-8/12 bg-green-200 h-full">
+          <div className=" w-8/12  mx-auto  bg-pink-700  border-2 border-gray-700 rounded-3xl">
+            <button onClick={(e) => backTrack(e)}className="absolute -top-3 -right-3">
+              <p className="text-xl bg-gray-300 pb-1 px-3 rounded-full">x</p>
+            </button>
+            <h1 className=' mt-8 text-base text-white mb-2'>Would you like to add a comment?</h1>
+          <div className="mx-4">
+            <label>
+              <textarea onChange={(e)=> setComment(e.target.value)} className="bg-gray-200 bg-opacity-50 w-full h-36 rounded-xl  text-xl px-1.5 resize-none" maxLength="100"></textarea>
+            </label>
+            <div className="inline-block w-full ">
+              {comment === "" ? (
+              <button className="float-left bg-gray-600 text-white cursor-not-allowed rounded-md ml-2 px-4 mt-2 ">
+                Send
+              </button>
+              ) : (
+                <button onClick={(e) => changeModal(e)}className="float-left bg-gray-200 rounded-md ml-2 px-4 mt-2 ">
+                Send
+              </button>
+              )}
+              <button onClick={(e) => changeModal(e)} className="float-right bg-gray-200 rounded-md mr-2 px-4 mt-2">
+                No
+              </button>
+            </div>
+          </div>
+            </div>  
         </div>
-      </div>
-        </div>  
       </div>
       ) : null}
     </>
