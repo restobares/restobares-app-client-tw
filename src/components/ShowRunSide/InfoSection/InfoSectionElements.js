43356e300afbba6f7f49d707f2a706696724c4be
chardhm <chych1317@gmail.com>
2022-01-12 19:38:38 -0600

    holis

--- src/components/ShowRunSide/InfoSection/InfoSectionElements.js
+++ src/components/ShowRunSide/InfoSection/InfoSectionElements.js
@@ -14,7 +14,7 @@ export const InfoWrapper = styled.div`
     display: grid;
     z-index: 1;
     height: 700px;
-    width: 100%;
+    width: 100% auto;
     max-width: 1100px;
     margin-right: auto;
     margin-left: auto;
